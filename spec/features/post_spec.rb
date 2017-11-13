2e8f1cb6e226bd609da9a7377de21da721d47199
bradmoaalii <brad.moaalii@gmail.com>
2017-11-13 14:38:43 -0700

    updated the post spec rb file, i missed this file update in a previous lesson

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -10,6 +10,32 @@ describe 'navigate' do
     it 'has a title of Posts' do
       visit posts_path
       expect(page).to have_content(/Posts/)
+   
+    end
+
+    it 'has a list of posts' do
+      post1=  = Post.create(date: Date.today, rationale: "Post1")
+      post2=  = Post.create(date: Date.today, rationale: "Post2")
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2)
+  end
+end
+
+Rspec.describe Post, type: :model do
+  describe "Creation" do
+    it 'can be created' do
+      post = Post.create(date: Date.today, rationale: "anything")
+      expect(post).to be_valid
+    end
+
+    it 'can be created' do
+      expect(@post).to be_valid
+    end
+
+    it 'cannot be created without a date and rationale' do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to be_valid
     end
   end
 end
