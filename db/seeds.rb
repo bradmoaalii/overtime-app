37b7a7195b0b76de5b81331fe77ef4d14ac0b701
bradmoaalii <brad.moaalii@gmail.com>
2017-11-21 11:44:49 -0700

    adjusted post dashboard and added admin user. conducted migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 @user = User.create!(email: "test@test.com", password: "swordfish", password_confirmation: "swordfish", first_name: "Jon", last_name: "Snow")
 
 puts "1 User created"
+
+AdminUser.create!(email: "admin@test.com", password: "swordfish", password_confirmation: "swordfish", first_name: "Admin", last_name: "LastNameUSer")
+
+
+puts "1 User created"
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
