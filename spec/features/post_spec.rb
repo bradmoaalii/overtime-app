31a35177cc8dae3f4885c5c394b2b3399633e745
bradmoaalii <brad.moaalii@gmail.com>
2017-11-29 12:46:04 -0700

    added hours requested to post model

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,7 +5,7 @@ describe 'navigate' do
   let(:user) { FactoryBot.create(:user) }
 
   let(:post) do
-    Post.create(date: Date.today, rationale: "asdfdfsd", user_id: user.id)
+    Post.create(date: Date.today, rationale: "asdfdfsd", user_id: user.id, overtime_request: 3.5)
   end
   
   before do
@@ -35,7 +35,7 @@ describe 'navigate' do
     it 'has a scope so that only post creators can see their posts' do
       other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "swordfish", password_confirmation: "swordfish")
       
-      post_from_other_user = Post.create(date: Date.today, rationale: "you do not see this screen", user_id: other_user.id)
+      post_from_other_user = Post.create(date: Date.today, rationale: "you do not see this screen", user_id: other_user.id, overtime_request: 3.5)
       expect(page).to have_content(/This post shouldnt be seen/)
     end
   end
@@ -56,7 +56,7 @@ describe 'navigate' do
       delete_user = FactoryBot.create(:user)
       login_as(delete_user, :scope => :user)
 
-      post_to_delete = Post.create(date: Date.today, rationale: 'hppy ddoobe ddo', user_id: delete_user.id)
+      post_to_delete = Post.create(date: Date.today, rationale: 'hppy ddoobe ddo', user_id: delete_user.id, overtime_request: 3.5)
 
       visit posts_path
 
@@ -77,14 +77,15 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-      click_on "Save"
+      fill_in 'post[overtime_request]', with: 4.5
 
-      expect(page).to have_content("Some rationale")
+      expect { click_on "Save" }.to change(Post, :count).by(1)
     end
 
     it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
+      fill_in 'post[overtime_request]', with: 4.5
       click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("User Association")
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,101 +1,25 @@
+require 'rails_helper'
 
-describe 'navigate' do
-  before do
-    @user = FactoryBot.create(:user)
-    login_as(@user, :scope => :user)
-  end
-
-  describe 'index' do
+RSpec.describe Post, type: :model do
+  describe "Creation" do
     before do
-      visit posts_path
+      @post = FactoryGirl.create(:post)
     end
 
-    it 'can be reached successfully' do
-      expect(page.status_code).to eq(200)
+    it 'can be created' do  
+      expect(@post).to be_valid
     end
 
-    it 'has a title of Posts' do
-      expect(page).to have_content(/Posts/)
+    it 'cannot be created without a date, rationale, and overtime_request' do
+      @post.date = nil
+      @post.rationale = nil
+      @post.overtime_request = nil
+      expect(@post).to_not be_valid
     end
 
-    it 'has a list of posts' do
-      post1 = FactoryBot.build_stubbed(:post)
-      post2 = FactoryBot.build_stubbed(:second_post)
-      visit posts_path
-      expect(page).to have_content(/Rationale|content/)
-    end
-  end
-
-  describe 'new' do
-    it 'has a link from the homepage' do
-      visit root_path
-
-      click_link("new_post_from_nav")
-      expect(page.status_code).to eq(200)
-    end
-  end
-
-  describe 'delete' do
-    it 'can be deleted' do
-      @post = FactoryBot.create(:post)
-      visit posts_path
-
-      click_link("delete_post_#{@post.id}_from_index")
-      expect(page.status_code).to eq(200)
-    end
-  end
-
-  describe 'creation' do
-    before do
-      visit new_post_path
-    end
-
-    it 'has a new form that can be reached' do
-      expect(page.status_code).to eq(200)
-    end
-
-    it 'can be created from new form page' do
-      fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "Some rationale"
-      click_on "Save"
-
-      expect(page).to have_content("Some rationale")
-    end
-
-    it 'will have a user associated it' do
-      fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "User Association"
-      click_on "Save"
-
-      expect(User.last.posts.last.rationale).to eq("User Association")
-    end
-  end
-
-  describe 'edit' do
-    before do
-      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-      login_as(@edit_user, :scope => :user)
-      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
-    end
-
-    it 'can be edited' do
-      visit edit_post_path(@edit_post)
-
-      fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "Edited content"
-      click_on "Save"
-
-      expect(page).to have_content("Edited content")
-    end
-
-    it 'cannot be edited by a non authorized user' do
-      logout(:user)
-      non_authorized_user = FactoryBot.create(:non_authorized_user)
-      login_as(non_authorized_user, :scope => :user)
-
-      visit edit_post_path(@edit_post)
-
-      expect(current_path).to eq(root_path)
+    it 'has an overtime_request greater than 0.0' do
+      @post.overtime_request = 0.0
+      expect(@post).to_not be_valid
     end
   end
 end
